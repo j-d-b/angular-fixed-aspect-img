a942870b6b4d39a4816ee3c7d1d58629d51f99d2
j-d-b <jacob@jdbrady.info>
2019-03-15 09:24:54 -0400

    refactor: fnc renaming, condensing

--- src/fixedAspectImg.controller.js
+++ src/fixedAspectImg.controller.js
@@ -13,42 +13,32 @@ export default function () {
 
   const getAspectDecimal = aspectRatio => aspectRatio ? parseAspectRatio(aspectRatio) : 0.75; // default to 4:3
 
-  const sizeImage = (image, aspectRatioDecimal) => {
+  const fitImage = (image, aspectRatioDecimal, container) => {
     if (image.height/image.width > aspectRatioDecimal) {
       // bars on side
       image.style.height = '100%';
       image.style.width = '';
+      image.style.transform = `translateX(${(container.offsetWidth-image.width)/2}px)`;
     } else {
       // bars on top/bot
       image.style.height = '';
       image.style.width = '100%';
-    }
-  };
-
-  const centerImage = (image, aspectRatioDecimal, container) => {
-    if (image.height/image.width > aspectRatioDecimal) {
-      // bars on side
-      image.style.transform = `translateX(${(container.offsetWidth-image.width)/2}px)`;
-    } else {
-      // bars on top/bot
       image.style.transform = `translateY(${(container.offsetHeight-image.height)/2}px)`;
     }
   };
 
-  const sizeAll = isInit => {
+  const fitAll = isInit => {
     const aspectDecimal = getAspectDecimal(this.aspectRatio);
 
     document.querySelectorAll('.fai-container').forEach(container => {
       container.style.height = container.offsetWidth * aspectDecimal + 'px';
 
       const image = container.querySelector('.fai-image');
-      sizeImage(image, aspectDecimal);
-      centerImage(image, aspectDecimal, container);
+      fitImage(image, aspectDecimal, container);
 
       if (isInit) {
         image.addEventListener('load', () => {
-          sizeImage(image, getAspectDecimal(this.aspectRatio));
-          centerImage(image, getAspectDecimal(this.aspectRatio), container);
+          fitImage(image, getAspectDecimal(this.aspectRatio), container);
         });
       }
     });
@@ -56,14 +46,14 @@ export default function () {
 
   this.$onInit = () => {
     angular.element(document).ready(() => {
-      sizeAll(true);
+      fitAll(true);
     });
-    window.addEventListener('resize', sizeAll);
+    window.addEventListener('resize', fitAll);
   };
 
   this.$onChanges = changes => {
     if (changes.aspectRatio) {
-      sizeAll();
+      fitAll();
     }
   };
 }
